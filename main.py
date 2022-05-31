ede504eaae522bb40e98330ee682eb3c72b1de9e
geddie212 <geddie212>
2022-05-31 15:07:14 +0100

    sql uri testing

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import os
 
 app = Flask(__name__)
 
-SQL = os.environ.get('DATABASE_URL').split('postgres')
-SQL[0] += 'postgresql'
-updated_SQL = f'{SQL[0]}{SQL[1]}'
+# SQL = os.environ.get('DATABASE_URL').split('postgres')
+# SQL[0] += 'postgresql'
+# updated_SQL = f'{SQL[0]}{SQL[1]}'
+updated_SQL = 'postgresql://wskrrwbfsimlao:395b197cdb774d7d3c1d265e3433555df463a6995694da95eedb5e3e918909d5@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/dfdsggdmc8feet'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = updated_SQL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
 # SECRET KEY SETUP
-app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
-
+# app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+app.config["SECRET_KEY"] = '349o2dx7r86tbhn47c55476hnrytc26h734dh78924rd36g8'
+db = SQLAlchemy(app)
 
 filters = dict(location='all', sockets='all', wifi='all', toilet='all', calls='all', seats='all', coffee_price='all')
 img_selected = False
